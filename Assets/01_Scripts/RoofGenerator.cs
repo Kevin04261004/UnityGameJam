0ca03de2b501734ecb8648d20b1fdb953480a2c7
elsgundam <elsgundam.dev@gmail.com>
2024-07-17 11:38:52 +0900

    roof generator 수정

--- Assets/01_Scripts/RoofGenerator.cs
+++ Assets/01_Scripts/RoofGenerator.cs
@@ -12,60 +12,66 @@ public class RoofGenerator : MonoBehaviour
     [SerializeField] private List<Sprite> roofSprites;
     [SerializeField] private List<Transform> points; // Vector2 , 그래프 편집 툴 전환 필요
     private List<Falling_Object> _fallingObjects;
-    
-    private void Awake()
-    {
-        if (fallingObjectPrefab == null)
-        {
-            Debug.LogWarning("Prefab is not exist!!");
-            return;
-        }
 
-        if (_fallingObjects == null)
-        {
-            _fallingObjects = new List<Falling_Object>();
-        }
-        _fallingObjects.Clear();
-        
-        // sort points by x pos 
-        points.Sort((transform1, transform2) => { return transform1.position.x.CompareTo(transform2.position.x);});
-        
-        
-        //AnimationCurve
+    [Header("Roof Stats")] [SerializeField]
+    private float fallSpeedBase = 2;
+   [SerializeField] private int variableRange = 80;
 
-        float width =  fallingObjectPrefab.transform.localScale.x;
 
-        for (int i = 0; i < points.Count - 1; i++)
-        {
-            int interval = (int)((points[i + 1].position.x - points[i].position.x) / width);
-            
-            float yInterval =  (points[i + 1].position.y - points[i].position.y) / interval;
-            float xInterval = (points[i + 1].position.x - points[i].position.x) / interval;
-            
-            for (int j = 0; j < interval; j++)
-            {
-                Falling_Object fallingObject = Instantiate(fallingObjectPrefab);
-                GameObject go = fallingObject.gameObject;
-                int rand = Random.Range(0, 2);
-                go.GetComponent<SpriteRenderer>().sprite = roofSprites[rand];
-                go.GetComponent<SpriteRenderer>().color = rand == 0 ? Color.white : Color.red;
-                
-                float xPos = xInterval *  j;
-                float yPos = yInterval *  j;
-                go.transform.position = points[i].position +  new Vector3(xPos, yPos);
-                go.gameObject.name = $"FallObject_{i}_{j}";
-                
-                _fallingObjects.Add(fallingObject);
+   private void Awake()
+   {
+       if (fallingObjectPrefab == null)
+       {
+           Debug.LogWarning("Prefab is not exist!!");
+           return;
+       }
 
-            }
-            
-            
-        }
-        
-        
-    }
+       if (_fallingObjects == null)
+       {
+           _fallingObjects = new List<Falling_Object>();
+       }
 
-    private void Start()
+       _fallingObjects.Clear();
+
+       // sort points by x pos 
+       points.Sort((transform1, transform2) => { return transform1.position.x.CompareTo(transform2.position.x); });
+
+
+       //AnimationCurve
+
+       float width = fallingObjectPrefab.transform.localScale.x;
+
+       for (int i = 0; i < points.Count - 1; i++)
+       {
+           int interval = (int)((points[i + 1].position.x - points[i].position.x) / width);
+
+           float yInterval = (points[i + 1].position.y - points[i].position.y) / interval;
+           float xInterval = (points[i + 1].position.x - points[i].position.x) / interval;
+
+           for (int j = 0; j < interval; j++)
+           {
+               Falling_Object fallingObject = Instantiate(fallingObjectPrefab);
+               GameObject go = fallingObject.gameObject;
+               int rand = Random.Range(0, 2);
+               go.GetComponent<SpriteRenderer>().sprite = roofSprites[rand];
+               go.GetComponent<SpriteRenderer>().color = rand == 0 ? Color.white : Color.red;
+
+               float xPos = xInterval * j;
+               float yPos = yInterval * j + (Random.Range(-variableRange, variableRange) / 100f);
+               go.transform.position = points[i].position + new Vector3(xPos, yPos);
+               go.gameObject.name = $"FallObject_{i}_{j}";
+
+               _fallingObjects.Add(fallingObject);
+
+           }
+
+
+       }
+
+
+   }
+
+   private void Start()
     {
         StartCoroutine(IFallObjects(new WaitForSeconds(0.2f)));
     }
