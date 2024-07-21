34d6dd0d95439b484ad9f8e68fbd9ab6c47522a7
kimdoyoon <kdystudy0426@gmail.com>
2024-07-21 17:40:55 +0900

    Live 2D

--- Assets/01_Scripts/Live2D/CharacterLookAt.cs
+++ Assets/01_Scripts/Live2D/CharacterLookAt.cs
@@ -10,17 +10,17 @@ public class CharacterLookAt : MonoBehaviour
     public Transform startTr;
     private Camera cam;
     public CubismModel CubismModel;
+    public bool isBlinked = false;
     public enum EParamType
     {
         headX = 0, // -30 ~ 30
         headY, // -30 ~ 30
         headZ, // -30 ~ 30
-        smileLeft, // -1 ~ 1
         blinkLeft, // -1 ~ 1
         blinkRight, // -1 ~ 1
-        smileRight, // -1 ~ 1
         eyeX, // -1 ~ 1
         eyeY, // -1 ~ 1
+        breath,
         frontHairX, // -1 ~ 1
         frontHairY, // -1 ~ 1
         backHairX, // -1 ~ 1
@@ -31,8 +31,7 @@ public class CharacterLookAt : MonoBehaviour
     public CubismParameter[] Parameters;
     public float maxDistance = 1;
     public float blinkTime = 0;
-    private WaitForSeconds blinkWFS = new WaitForSeconds(0.3f);
-    private Coroutine blinkCoroutine;
+    public float breathTime = 0;
     private void Awake()
     {
         cam = Camera.main;
@@ -44,7 +43,7 @@ public class CharacterLookAt : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         Vector2 pos = cam.ScreenToWorldPoint((Vector2)Input.mousePosition);
 
@@ -63,26 +62,31 @@ public class CharacterLookAt : MonoBehaviour
         Parameters[(int)EParamType.eyeX].Value = dir.x * (distance / maxDistance);
         Parameters[(int)EParamType.eyeY].Value = dir.y * (distance / maxDistance);
 
-        // blinkTime += Time.deltaTime;
-        // if (blinkTime > 1f) //Random.Range(0.5f, 1f))
-        // {
-        //     blinkTime = 0;
-        //     if (blinkCoroutine == null)
-        //     {
-        //         blinkCoroutine = StartCoroutine(Blink());
-        //     }
-        // }
+        blinkTime -= Time.deltaTime;
+        if (blinkTime <= 0)
+        {
+            Parameters[(int)EParamType.blinkLeft].Value = isBlinked ? 1 : 0;
+            Parameters[(int)EParamType.blinkRight].Value = isBlinked ? 1 : 0;
+
+            blinkTime = isBlinked ? Random.Range(0.2f, 0.3f) : Random.Range(1,1.5f);
+            isBlinked = !isBlinked;
+        }
+        
+        breathTime -= Time.deltaTime;
+        if (breathTime <= 0)
+        {
+            if (Parameters[(int)EParamType.breath].Value == 1)
+            {
+                Parameters[(int)EParamType.breath].Value = -1;
+            }
+            else
+            {
+                Parameters[(int)EParamType.breath].Value = 1;
+            }
+        
+            breathTime = 0.5f;
+        }
     }
-    //
-    // private IEnumerator Blink()
-    // {
-    //     Parameters[(int)EParamType.blinkLeft].Value = 0f;
-    //     Parameters[(int)EParamType.blinkRight].Value = 0f;
-    //     yield return blinkWFS;
-    //     Parameters[(int)EParamType.blinkLeft].Value = 1f;
-    //     Parameters[(int)EParamType.blinkRight].Value = 1f;
-    //
-    // }
-    
-    
+
+
 }
